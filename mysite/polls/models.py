f9f3ad4c465ac9bca710d91982da6aaef75c7f59
Alex Lue <a.lue820@gmail.com>
2019-09-06 15:28:41 -0700

    broke up language check into a separate function that loads yaml and added comments

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -8,13 +8,28 @@ from django.utils.translation import gettext_lazy as _
 import yaml
 
 
-def language_check(sentence):
+def load_yaml_blacklist():
+    '''
+    loads list into a set and returns
+    '''
     with open('polls/blacklist.yaml', 'r') as f:
         try:
-            blacklist = list(yaml.safe_load(f)['blacklisted-words'])
+            blacklist = set(yaml.safe_load(f)['blacklisted-words'])
         except yaml.YAMLError as e:
             print(e)
-    punctuation = "?!.'\""
+
+    return blacklist
+
+
+def language_check(sentence):
+    '''
+    language_check gets passed a sentence. the function will
+    strip casing, remove common punctuation, and checks the word
+    with the blacklisted words in the yaml file
+    returns: (True, coarse_word) OR (False, None)
+    '''
+    blacklist = load_yaml_blacklist()
+    punctuation = '?!.\'"'
     for word in sentence.split():
         for punct in punctuation:
             word = word.replace(punct, '')
