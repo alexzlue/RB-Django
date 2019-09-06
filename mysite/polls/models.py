10126cef8fd38d28dacea9e9322a3294eebcff09
Alex Lue <a.lue820@gmail.com>
2019-09-06 15:07:23 -0700

    changed pre-save checks. override 'clean' method in models to detect invalid words

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,22 +1,25 @@
 import datetime
 
 from django.db import models
-from django.core.exceptions import FieldError
+from django.core.exceptions import ValidationError
 from django.utils import timezone
 from django.dispatch import receiver
-from django.db.models.signals import pre_save
+from django.utils.translation import gettext_lazy as _
 import yaml
 
 
 def language_check(sentence):
     with open('polls/blacklist.yaml', 'r') as f:
         try:
-            blacklist = set(yaml.safe_load(f)['blacklisted-words'])
+            blacklist = list(yaml.safe_load(f)['blacklisted-words'])
         except yaml.YAMLError as e:
             print(e)
+    punctuation = "?!.'\""
     for word in sentence.split():
+        for punct in punctuation:
+            word = word.replace(punct, '')
         if word.lower() in blacklist:
-            return True, word
+            return True, word.lower()
     return False, None
 
 
@@ -31,33 +34,29 @@ class Question(models.Model):
     was_published_recently.boolean = True
     was_published_recently.short_description = 'Published recently?'
 
-    # def save(self, *args, **kwargs):
-    #     print('snoteuhisnoteuhentoihneouth')
-    #     print(self.question_text)
-    #     if not language_check(self.question_text):
-    #         print('to be saved')
-    #         super(Question, self).save(*args, **kwargs)
-    #     else:
-    #         print('not saved')
-    #         raise Exception("AHHHHH")
+    def clean(self):
+        question = self.question_text
+        value = language_check(question)
+        # print(value)
+        if value[0]:
+            raise ValidationError(_(
+                    'Coarse words like ' + value[1] + ' are not allowed.'))
 
     def __str__(self):
         return self.question_text
-
-
-@receiver(pre_save, sender=Question)
-def q_has_coarse_language(sender, instance, *args, **kwargs):
-    sentence = instance.question_text
-    error = language_check(sentence)
-    if error[0]:
-        raise FieldError('Cannot Enter this selection because \
-            you used the word: ' + error[1])
 
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+
+    def clean(self):
+        choice = self.choice_text
+        value = language_check(choice)
+        if value[0]:
+            raise ValidationError(_(
+                    'Coarse words like ' + value[1] + ' are not allowed.'))
 
     def __str__(self):
         return self.choice_text
