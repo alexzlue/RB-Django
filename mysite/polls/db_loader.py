93bab033be4bff0da11fcaf2fb97ea2704ef9526
Alex Lue <a.lue820@gmail.com>
2019-09-10 12:59:11 -0700

    db loader that adds companies to db

---
+++ mysite/polls/db_loader.py
@@ -0,0 +1,15 @@
+from django.utils import timezone
+
+from .helper import load_yaml_fortune
+from .models import Company
+
+
+def load_companies():
+    '''
+    Load every company into the database
+    '''
+    company_list = load_yaml_fortune()
+    for company in company_list:
+        now = timezone.now()
+        c = Company(name=company, type='B2A', created_at=now, updated_at=now)
+        c.save()
