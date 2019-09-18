383f87bae9b89964f029b6c1a1f76da59b9fcd61
Alex Lue <a.lue820@gmail.com>
2019-09-18 11:46:04 -0700

    added constant-based number of choices requirement for questions

--- mysite/polls/forms.py
+++ mysite/polls/forms.py
@@ -8,9 +8,12 @@ from .custom_layout import *
 from .models import Company, Question, Choice
 from .helper import language_filter
 
+REQ_NUM_CHOICES = 2
+
 
 class ChoiceForm(forms.ModelForm):
     ''' Form for choices '''
+
     class Meta:
         model = Choice
         fields = '__all__'
@@ -60,8 +63,28 @@ class CreateForm(forms.ModelForm):
         language_filter(self.cleaned_data['question_text'])
         return self.cleaned_data['question_text']
 
+
+class RequiredFormSet(forms.BaseInlineFormSet):
+
+    def clean(self):
+        if self.is_valid():
+            cleaned_data = self.cleaned_data
+            valid_forms = 0
+            for choice in cleaned_data:
+                if choice:
+                    try:
+                        if choice['choice_text']:
+                            valid_forms += 1
+                    except KeyError:
+                        pass
+            if valid_forms < REQ_NUM_CHOICES:
+                raise forms.ValidationError("Submit at least " +
+                                            str(REQ_NUM_CHOICES) +
+                                            " choices!")
+
 CHOICE_FORM_SET = forms.inlineformset_factory(Question,
                                               Choice,
                                               form=ChoiceForm,
+                                              formset=RequiredFormSet,
                                               fields=['choice_text'],
-                                              extra=3)
+                                              extra=REQ_NUM_CHOICES)
